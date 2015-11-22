5ccd2308fea9f3c6322063c3e1b44dde45b68707
Hancel.Lin <imlinhanchao@gmail.com>
2015-11-22 15:56:59 +0800

    修改测试Window Form

--- Test/resource.h
+++ Test/resource.h
@@ -8,6 +8,9 @@
 #define IDC_EDIT1                       1001
 #define IDC_EDIT2                       1002
 #define IDC_EDIT3                       1003
+#define IDC_CHECK1                      1004
+#define IDC_RADIO1                      1005
+#define IDC_RADIO2                      1006
 
 // Next default values for new objects
 // 
@@ -15,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
